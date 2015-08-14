d45143b40b1abab7c119f96fdd4a77576fb619f5
btoal <btoal@salesforce.com>
2015-08-13 23:42:35 -0700

    Initial revision

---
+++ src/main/java/wts/Main.java
@@ -0,0 +1,32 @@
+package wts;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		String webappDirLocation = "src/main/webapp/";
+
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
+
+		System.setProperty("org.eclipse.jetty.LEVEL=DEBUG", "true");
+		Server server = new Server(Integer.valueOf(webPort));
+		WebAppContext root = new WebAppContext();
+
+		root.setContextPath("/");
+		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		root.setResourceBase(webappDirLocation);
+
+		// http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+		root.setParentLoaderPriority(true);
+
+		server.setHandler(root);
+
+		server.start();
+		server.join();
+	}
+}
