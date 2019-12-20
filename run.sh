2fbb3def32e9e6eaeaf168d42035db088a75f2b6
Brian Toal <brian.toal@gmail.com>
2019-12-20 00:54:29 -0800

    Fixed depracated Xbootclasspath

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo working dir = `pwd`
-java -Xbootclasspath/p:target/libs/alpn-boot-8.1.6.v20151105.jar -jar target/wts-1-SNAPSHOT.jar 
+java --patch-module java.base=target/libs/alpn-boot-8.1.6.v20151105.jar --add-exports java.base/org.eclipse.jetty.alpn=ALL-UNNAMED -jar target/wts-1-SNAPSHOT.jar 
